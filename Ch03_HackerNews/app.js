3e6a565c1cb076efcd96497eea9568754383d33f
soo <gjstn0308@naver.com>
2023-04-11 22:39:17 +0900

    tailwind cdn

--- Ch03_HackerNews/app.js
+++ Ch03_HackerNews/app.js
@@ -4,9 +4,8 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const store = {
-  currentPage : 1,
-
-}
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -18,10 +17,22 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
+  let template = `
+  <div class="container mx-auto p-4">
+    <h1>Hacker News</h1>
+    <ul>
+      <li>
+        {{__news_feed__}}
+      </li> 
+    </ul>
+    <div>
+      <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+      <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    </div>
+  </div>
+  `;
 
-  newsList.push("<ul>");
-
-  for (let i = (store.currentPage-1)*10; i < store.currentPage*10  ; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li> 
       <a href="#/show/${newsFeed[i].id}">
@@ -33,39 +44,38 @@ function newsFeed() {
     `);
   }
 
-  newsList.push("</ul>");
-newsList.push(`
-<div>
-<a href="#/page/${store.currentPage > 1 ? store.currentPage -1 : 1 }">이전페이지 </a>
-<a href="#/page/${store.currentPage +1}">다음페이지 </a>
-</div>
-`)
-  container.innerHTML = newsList.join('');
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace("{{__next_page__}}", store.currentPage + 1);
+
+  container.innerHTML = template;
 }
 
 function newsDetail() {
   const id = location.hash.substring(7);
-  const newsContent = getData(CONTENT_URL.replace('@id', id))
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
-    <h1> ${(newsContent.title)}</h1>
-  
-    <div> 
-    <a  href="#/page/${store.currentPage}"> 목록으로 </a>
+    <h1> ${newsContent.title}</h1>
+    <div>
+      <a  href="#/page/${store.currentPage}"> 목록으로 </a>
     </div>
-    `;
+              `;
 }
 
 function router() {
   const routePath = location.hash;
 
-  if (routePath === '') {
+  if (routePath === "") {
     newsFeed();
-  } else if(routePath.indexOf('#/page/') >= 0 ) {
+  } else if (routePath.indexOf("#/page/") >= 0) {
     store.currentPage = Number(routePath.substring(7));
     newsFeed();
-  }else{
-    newsDetail()
+  } else {
+    newsDetail();
   }
 }
 
